0dcf18c864b71564efc78d8910ff66eed34bb623
@laicanw <laicanw@yonyou>
2023-03-24 10:20:02 +0800

    分支合并

--- mallDemo/src/main/java/com/example/malldemo/controller/UserController.java
+++ mallDemo/src/main/java/com/example/malldemo/controller/UserController.java
@@ -1,17 +1,11 @@
 package com.example.malldemo.controller;
 
-<<<<<<< HEAD
 import com.alibaba.fastjson.JSONObject;
-=======
->>>>>>> f43a413 (商场Demo首次上传)
 import com.example.malldemo.entity.User;
 import com.example.malldemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.RequestBody;
-=======
->>>>>>> f43a413 (商场Demo首次上传)
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -30,18 +24,11 @@ public class UserController {
     ModelAndView modelAndView = new ModelAndView();
 
     @PostMapping("/login")
-<<<<<<< HEAD
     public ModelAndView login(@RequestBody JSONObject jsonObject){
         User user = JSONObject.toJavaObject(jsonObject,User.class);
 //        Map<String, String> map = userService.selectUserByName(user.getUserName());
 //        modelAndView.addObject(map);
 //        modelAndView.setViewName("index");
-=======
-    public ModelAndView login(@RequestParam User user){
-        Map<String, String> map = userService.selectUserByName(user.getUserName());
-        modelAndView.addObject(map);
-        modelAndView.setViewName("index");
->>>>>>> f43a413 (商场Demo首次上传)
         return modelAndView;
     }
 }
